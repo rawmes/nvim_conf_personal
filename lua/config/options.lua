b22c9fe09a50d03c23583283e0d0b34643234124
Ramesh Dahal <ramesh@callbreak.com>
2025-02-04 20:44:00 +0545

    added color scheme and few key maps changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,3 +13,7 @@ o.numberwidth = 1
 o.expandtab = false
 o.cursorcolumn = true
 o.cursorline = true
+
+-- In case you don't want to use `:LazyExtras`,
+-- then you need to set the option below.
+vim.g.lazyvim_picker = "telescope"
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,62 @@
 return {
 	{
-		enabled = false,
+		enabled = true,
 		"scottmckendry/cyberdream.nvim",
 		lazy = false,
 		name = "cyberdream",
+		config = function()
+			require("cyberdream").setup({
+				transparent = true,
+				italic_comments = true,
+				hide_fillchars = true,
+
+				borderless_telescope = false,
+
+				-- Set terminal colors used in `:terminal`
+				terminal_colors = true,
+
+				-- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+				cache = false,
+
+				theme = {
+					variant = "auto", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+					saturation = 1, -- accepts a value between 0 and 1. 0
+					overrides = function(colors)
+						return {
+							Comment = {
+								fg = colors.grey,
+								bg = "NONE",
+								italic = true,
+							},
+							Constant = {
+								fg = colors.red,
+								bg = "NONE",
+								bold = true,
+							},
+							Todo = {
+								fg = "#000000",
+								bg = colors.blue,
+								bold = true,
+							},
+							Function = {
+								italic = true,
+							},
+							["@property"] = { fg = colors.yellow },
+						}
+					end,
+				},
+
+				-- Disable or enable colorscheme extensions
+				extensions = {
+					telescope = true,
+					notify = false,
+					mini = true,
+				},
+			})
+		end,
 	},
 	{
-		enabled = true,
+		enabled = false,
 		-- there is no cat here
 		"catppuccin/nvim",
 		lazy = false,
@@ -15,7 +65,7 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
+			colorscheme = "cyberdream",
 		},
 	},
 }
